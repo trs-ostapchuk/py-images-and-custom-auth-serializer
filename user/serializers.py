fe2cb7ff920b4d5f12f7a14fb605665f233c1290
Taras <tarasostapchuk27@gmail.com>
2026-01-19 20:49:56 +0100

    Fix AuthTokenSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -49,22 +49,24 @@ class AuthTokenSerializer(serializers.Serializer):
     )
 
     def validate(self, attrs):
-        email = attrs.get("username")
+        email = attrs.get("email")
         password = attrs.get("password")
 
         if email and password:
-            user = authenticate(request=self.context.get("request"),
-                                email=email, password=password)
+            email = authenticate(
+                request=self.context.get("request"),
+                email=email, password=password
+            )
 
             # The authenticate call simply returns None for is_active=False
             # users. (Assuming the default ModelBackend authentication
             # backend.)
-            if not user:
+            if not email:
                 msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg, code="authorization")
         else:
             msg = _("Must include 'username' and 'password'.")
             raise serializers.ValidationError(msg, code="authorization")
 
-        attrs["user"] = user
+        attrs["email"] = email
         return attrs
