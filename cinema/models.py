d04d9e98ce9dd3b848db9109efcbec2dc099230b
Taras <tarasostapchuk27@gmail.com>
2026-01-19 21:19:32 +0100

    Fix movie_image_path

--- cinema/models.py
+++ cinema/models.py
@@ -45,7 +45,7 @@ def movie_image_path(instance: "Movie", filename: str) -> pathlib.Path:
     """
     filename = (f"{slugify(instance.title)}-{uuid.uuid4()}"
                 + pathlib.Path(filename).suffix)
-    return pathlib.Path("upload/movies/") / pathlib.Path(filename)
+    return pathlib.Path("uploads/movies/") / pathlib.Path(filename)
 
 
 class Movie(models.Model):
