91e845358d118ef9a26235e2c56944b07589d14a
Taras <tarasostapchuk27@gmail.com>
2026-01-19 20:51:10 +0100

    Fix PEP8 in MovieSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -33,7 +33,15 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors", "image")
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors",
+            "image"
+        )
 
 
 class MovieImageSerializer(serializers.ModelSerializer):
